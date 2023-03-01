12c9f8acc7ec0925be60c7ce5a01f3cb2cb4c677
Marcin Panasiuk <panasiuk.marcin@gmail.com>
2023-03-01 22:56:57 +0100

    Add database config and better psycopg query creation

---
+++ backend/src/database/config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+host = environ["DB_HOST"]
+port = environ["DB_PORT"]
+name = environ["DB_NAME"]
+user = environ["DB_USER"]
+password = environ["DB_PASSWORD"]
--- backend/src/database/simple.py
+++ backend/src/database/simple.py
@@ -1,15 +1,14 @@
 from os import environ
 import psycopg
-from psycopg import DatabaseError
+from psycopg import DatabaseError, sql
 import json
+import database.config as config
 
 db_user = environ["DB_USER"]
 db_password = environ["DB_PASSWORD"]
 db_name = environ["DB_NAME"]
 db_port = environ["DB_PORT"]
-connection_info = (
-    f"host=db port={db_port} dbname={db_name} user={db_user} password={db_password}"
-)
+connection_info = f"host={config.host} port={config.port} dbname={config.name} user={config.user} password={config.password}"
 
 
 def connect():
@@ -18,34 +17,34 @@ def connect():
 
 def try_init_board(room_id, board):
     with connect() as connection:
-        sql = f"""
+        query = f"""
             INSERT INTO BOARDS(room_uuid, board)
-            VALUES('{room_id}', '{board}')
+            VALUES(%s, %s)
             ON CONFLICT (room_uuid) DO NOTHING
         """
-        connection.execute(sql)
+        connection.execute(sql.SQL(query), [room_id, json.dumps(board)])
 
 
 def set_board(room_id, board):
     with connect() as connection:
-        sql = f"""
+        query = f"""
             INSERT INTO BOARDS(room_uuid, board)
-            VALUES('{room_id}', '{json.dumps(board)}')
+            VALUES(%s, %s)
             ON CONFLICT (room_uuid) DO UPDATE
                 SET room_uuid=excluded.room_uuid,
                     board=excluded.board
         """
-        connection.execute(sql)
+        connection.execute(sql.SQL(query), [room_id, json.dumps(board)])
 
 
 def get_board(room_id):
     with connect() as connection:
-        sql = f"""
+        query = f"""
             SELECT board FROM BOARDS
-            WHERE room_uuid = '{room_id}'
+            WHERE room_uuid = %s
         """
 
-        result = connection.execute(sql).fetchone()
+        result = connection.execute(sql.SQL(query), [room_id]).fetchone()
         if result:
             return json.loads(result[0])
         return None
@@ -53,11 +52,11 @@ def get_board(room_id):
 
 def join_player_with_color(connection, room, color):
     try:
-        sql = f"""
+        query = f"""
             INSERT INTO ROOMS(room, color)
-            VALUES('{room}', '{color}')
+            VALUES(%s, %s)
         """
-        connection.execute(sql)
+        connection.execute(sql.SQL(query), [room, color])
     except DatabaseError:
         connection.rollback()
         return False
